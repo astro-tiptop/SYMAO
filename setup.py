a194ee67d56b05fae2620109469d39914a2f8755
Lorenzo Busoni <lbusoni@gmail.com>
2022-03-02 22:10:20 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,25 @@
-from distutils.core import setup
+from setuptools import setup
+
+NAME = 'symao'
+DESCRIPTION = ''
+URL = 'https://github.com/FabioRossiArcetri/SYMAO'
+EMAIL = 'fabio.rossi@inaf.it'
+AUTHOR = 'Fabio Rossi'
+LICENSE = 'MIT'
+
+
 setup(name='symao',
+      description=DESCRIPTION,
+      url=URL,
+      author_email=EMAIL,
+      author=AUTHOR,
+      license=LICENSE,
       version='1.0',
-      py_modules=['symao'],
+      packages=['symao', ],
+      install_requires=[
+          "numpy",
+          "scipy",
+          "simpy",
+          "seeing",
+      ]
       )
